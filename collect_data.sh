a7aab4f349f95eff874d2c8648f4e886fdb22a0c
stargazerZJ <53366576+stargazerZJ@users.noreply.github.com>
2025-12-29 11:53:30 +0800

    feat: multigpu data collection

--- collect_data.sh
+++ collect_data.sh
@@ -2,12 +2,11 @@
 
 task_name=${1}
 task_config=${2}
-gpu_id=${3}
+gpu_ids=${3:-"0"}  # Default to GPU 0
 
 ./script/.update_path.sh > /dev/null 2>&1
 
-export CUDA_VISIBLE_DEVICES=${gpu_id}
-
 PYTHONWARNINGS=ignore::UserWarning \
-python script/collect_data.py $task_name $task_config
+python script/collect_data.py $task_name $task_config --gpu_ids $gpu_ids
+
 rm -rf data/${task_name}/${task_config}/.cache
